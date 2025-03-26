0ec7105bd66ee38f65ca2b7f14acf1b199ac22e8
Carl Mariño <97602174+carlmarinyo@users.noreply.github.com>
2025-03-26 20:47:15 +0800

    Latest 8:47pm 26/03/2025
    Ayos na yung taltong add

--- edit record and crime (record palang tapos/dbs.py
+++ edit record and crime (record palang tapos/dbs.py
@@ -3,109 +3,158 @@ from tkinter import messagebox
 def connectDb():
     conn = sqlite3.connect("criminal_records.db")
     cur = conn.cursor()
+    
+    #creation ng criminals table
+    cur.execute(""" 
+        CREATE TABLE IF NOT EXISTS criminals (
+        criminal_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        criminal_name TEXT NOT NULL ) """)
+
+    #creation ng crime table
     cur.execute("""
         CREATE TABLE IF NOT EXISTS crimes (
-            crime_Id INTEGER PRIMARY KEY AUTOINCREMENT,
-            crime_name TEXT NOT NULL,
+            crime_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            crime_name TEXT NOT NULL UNIQUE, 
             confinement INTEGER NOT NULL
         )
     """)
+
+    #creation ng records table
+
     cur.execute("""
         CREATE TABLE IF NOT EXISTS records (
-            ID INTEGER PRIMARY KEY,
-            name TEXT NOT NULL,
-            crime_Id INTEGER NOT NULL,
+            record_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            criminal_id INTEGER NOT NULL,
+            crime_id INTEGER NOT NULL,
             location TEXT NOT NULL,
-            date INTEGER NOT NULL,
-            mugshot BLOB NOT NULL,
-            FOREIGN KEY (crime_Id) REFERENCES crimes(crime_Id)
+            year_of_arrest INTEGER NOT NULL,
+            year_of_release INTEGER NOT NULL,
+            mugshot BLOB,
+            FOREIGN KEY (crime_id) REFERENCES crimes(crime_id),
+            FOREIGN KEY (criminal_id) REFERENCES criminals(criminal_id)
         )
     """)
+
+    ##auto update nung existing records kapag binago yung confinement years ng crime
+    cur.execute("""
+            CREATE TRIGGER IF NOT EXISTS update_year_of_release 
+            AFTER UPDATE OF confinement ON crimes
+            FOR EACH ROW
+            BEGIN
+                UPDATE records
+                SET year_of_release = year_of_arrest + NEW.confinement
+                WHERE crime_id = NEW.crime_id;
+            END;
+        
+    """)
+
+    cur.execute("""
+            CREATE TRIGGER IF NOT EXISTS update_release_on_arrest_change 
+            AFTER UPDATE OF year_of_arrest ON records
+            FOR EACH ROW
+            BEGIN
+                UPDATE records
+                SET year_of_release = NEW.year_of_arrest + (SELECT confinement from crimes WHERE crimes.crime_id = NEW.crime_id)
+                WHERE record_id = NEW.record_id;
+            END;
+        
+    """)
+
+
+     
     conn.commit()
     conn.close()
     
     
-def add_crimes():
+def add_crimes(cname, time):
     conn = sqlite3.connect("criminal_records.db")
     cur = conn.cursor()
-    crimes = [
-        ('Murder', 20),
-        ('Illegal Drug Trade', 12),
-        ('Theft', 2),
-        ('Assault', 3),
-        ('Fraud', 4),
-        ('Domestic Violence', 20),
-        ('Slander', 2),
-        ('Malicious Mischief', 6),
-        ('Cyber Libel', 4),
-        ('Identity Theft', 7)
-    ]
-    cur.executemany("INSERT INTO crimes (crime_name, confinement) VALUES (?, ?)", crimes)
+    cur.execute("INSERT INTO crimes(crime_name, confinement) VALUES (?, ?)",(cname,time))
     conn.commit()
     conn.close()
     
     
-def add_record(criminal_id, name, crime, location, date, mugshot):
-    conn = sqlite3.connect("criminal_records.db")
-    cur = conn.cursor()
-    cur.execute("INSERT INTO records (ID, name, crime, location, date, mugshot) VALUES (?, ?, ?, ?, ?, ?)", (criminal_id, name, crime, location, date, mugshot))
-    conn.commit()
-    conn.close()
+
     
 
 
 #carl methods
+def add_record(criminal_id, crime_id, location, date, mugshot): #updated
+    conn = sqlite3.connect("criminal_records.db")
+    cur = conn.cursor()
+
+    cur.execute("SELECT confinement from crimes WHERE crime_id = ?", (crime_id,))
+    result = cur.fetchone()
+
+    if result:
+        confinement = result[0]
+        releaseDate = int(date) + confinement
+
+        cur.execute("INSERT INTO records (criminal_id, crime_id, location, year_of_arrest, year_of_release, mugshot) VALUES (?, ?, ?, ?, ?, ?)", (criminal_id, crime_id, location, date, releaseDate, mugshot))
+        conn.commit()
+        conn.close()
+
+def checkEmpty(table_name): #checker if may laman tables
+    conn = sqlite3.connect("criminal_records.db")
+    cur = conn.cursor()
+    cur.execute(f"SELECT COUNT(*) FROM {table_name}")
+    count = cur.fetchone()[0]
+    conn.close()
+    return count == 0
 
 def checkExist(criminal_id):
     conn = sqlite3.connect("criminal_records.db")
     cur = conn.cursor()
 
     cur.execute("SELECT * FROM records WHERE ID = (?)",(criminal_id,))
-    result = cur.fetchone()
+    result = cur.fetchone()[0]
+    if result == 0:
+         return None
+
 
     conn.close()
     return result is not None
 
-def getCriminalData(criminal_id):
-    conn = sqlite3.connect("criminal_records.db")
-    cur = conn.cursor()
+# def getCriminalData(criminal_id):
+#     conn = sqlite3.connect("criminal_records.db")
+#     cur = conn.cursor()
 
-    cur.execute("SELECT ID, name, crime, location, date FROM records WHERE ID = ?",(criminal_id,))
-    result = cur.fetchone()
-    conn.close()
+#     cur.execute("SELECT location, year FROM records WHERE ID = ?",(criminal_id,))
+#     result = cur.fetchone()
+#     conn.close()
 
-    if result:
-        ID, name, crime, location, date = result
-        return {
-        "ID": ID,
-        "name": name,
-        "crime": crime,
-        "location": location,
-        "date": date
-        }
-    else:
-        return None
+#     if result:
+#         location, date = result
+#         return {
+#         "ID": ID,
+#         "name": name,
+#         "crime": crime,
+#         "location": location,
+#         "date": date
+#         }
+#     else:
+#         return None
     
 def addCriminal(cname):
         conn = sqlite3.connect("criminal_records.db")
         cur = conn.cursor()
-        cur.execute("INSERT INTO Criminals (name) VALUES (?)", (cname,))
+        cur.execute("INSERT INTO Criminals (criminal_name) VALUES (?)", (cname,))
         conn.commit()
         conn.close()
         
 
-def editRecord(criminal_id, criminal_name, criminal_desc, criminal_location, criminal_date):
+def editRecord(record_id, criminal_location, arrest_date): #update mugshot?? di ko alam pano to kirk
         conn = sqlite3.connect("criminal_records.db")
         cur = conn.cursor()
-        cur.execute("UPDATE records SET name = ?, crime = ?, location = ?, date = ? WHERE id = ?", (criminal_name, criminal_desc, criminal_location, criminal_date, criminal_id))
+        cur.execute("UPDATE records SET location = ?, year_of_arrest = ? WHERE id = ?", (criminal_location, arrest_date, record_id))
         conn.commit()
         conn.close()
 
 def getCriminalList():
      conn = sqlite3.connect("criminal_records.db")
      cur = conn.cursor()
-     cur.execute("SELECT name FROM Criminals")
-     criminals = [row[0] for row in cur.fetchall()]
+     cur.execute("SELECT criminal_id, criminal_name FROM criminals")
+     criminals = [(row[0],row[1]) for row in cur.fetchall()] #row - for id, row 1 for criminal name
      conn.close()
 
      if not criminals:
@@ -117,8 +166,13 @@ def getCriminalList():
 def getCrimeList():
      conn = sqlite3.connect("criminal_records.db")
      cur = conn.cursor()
-     cur.execute("SELECT crime_name FROM crimes")
-     crimes = [row[0] for row in cur.fetchall()]
+     cur.execute("SELECT crime_id, crime_name FROM crimes")
+     crimes = [(row[0], row[1]) for row in cur.fetchall()] #row 0 for id, row 1 for crime name
      conn.close()
+     if not crimes:
+          messagebox.showwarning("No crimes found","Crimes list is empty. Please add a crime first.")
+          return None
      return crimes
+
+
     #end of carl methods
--- edit record and crime (record palang tapos/interface.py
+++ edit record and crime (record palang tapos/interface.py
@@ -2,11 +2,14 @@ import tkinter as tk
 import random
 import dbs
 from tkinter import messagebox
-
+import sqlite3
 
 def addRecord():
-    criminal_list = dbs.getCriminalList()
-    if (criminal_list):
+    if dbs.checkEmpty("criminals") or dbs.checkEmpty("crimes"):
+        messagebox.showerror("Missing Data", "Either the Criminals or Crimes table is empty. Please add records first.")
+    else:
+        criminal_list = dbs.getCriminalList()
+    
         add_window = tk.Toplevel()
         add_window.title("Add Record")
         add_window.geometry("800x400+600+50")
@@ -22,7 +25,7 @@ def addRecord():
         criminal_var.set("Select Criminal")  # Default value
         criminals_list = dbs.getCriminalList()
         tk.Label(frame, text="Criminal:", font=("Arial", 14, "bold")).grid(row=1, column=0, padx=10, pady=10, sticky="w")
-        crime_dropdown = tk.OptionMenu(frame, criminal_var, *criminals_list)
+        crime_dropdown = tk.OptionMenu(frame, criminal_var, *[f"{id} - {name}" for id, name in criminal_list])
         crime_dropdown.grid(row=1, column=1, padx=10, pady=10)
         crime_dropdown.config(width=30, bd=3)
         
@@ -31,36 +34,41 @@ def addRecord():
         crime_var.set("Select Crime")
         crime_list = dbs.getCrimeList()
         tk.Label(frame, text="Crime:", font=("Arial", 14, "bold")).grid(row=2, column=0, padx=10, pady=10, sticky="w")
-        crime_dropdown = tk.OptionMenu(frame, crime_var, *crime_list)
+        crime_dropdown = tk.OptionMenu(frame, crime_var, * [f"{id} - {name}" for id, name in crime_list])
         crime_dropdown.grid(row=2, column=1, padx=10, pady=10)
         crime_dropdown.config(width=30, bd=3)
         
         #Location
-        tk.Label(frame, text="Enter Location: ", font=("Arial", 14, "bold")).grid(row=3, column=0, padx=5, pady=10, sticky="w")
+        tk.Label(frame, text="Location: ", font=("Arial", 14, "bold")).grid(row=3, column=0, padx=5, pady=10, sticky="w")
         criminalLocation = tk.Entry(frame, width=30, bd=3)
         criminalLocation.grid(row=3, column=1, padx=2, pady=10)
         
         #Date
-        tk.Label(frame, text="Enter Date(YEAR): ", font=("Arial", 14, "bold")).grid(row=4, column=0, padx=5, pady=10, sticky="w")
+        tk.Label(frame, text="Year of arrest: ", font=("Arial", 14, "bold")).grid(row=4, column=0, padx=5, pady=10, sticky="w")
         recordDate = tk.Entry(frame, width=30, bd=3)
         recordDate.grid(row=4, column=1, padx=2, pady=10)
+
+        date = recordDate.get()
         
         def add_to_db():
-            name = criminalName.get().strip()
-            crime = crime_var.get().strip()
+            criminal_id = criminal_var.get().strip(" - ")[0] #criminal id
+            crime_id = crime_var.get().strip(" - ")[0]
             location = criminalLocation.get().strip()
             date = recordDate.get().strip()
-            
-            if name == "" or location == "" or date == "":
-                messagebox.showerror("Error", "Name, Location and Date cannot be empty!")
+            blobplaceholder = None #temporary
+            if criminal_var.get() == "Select Criminal" or crime_var.get() =="Select Crime" or location == "" or date == "":
+                messagebox.showerror("Error", "Please complete the details of the record")
                 return
             
-            dbs.add_record(randNum, name, crime, location, date)
-            messagebox.showinfo("Succes", "Record added successfully!")
-            add_window.destroy()
-
-        submit = tk.Button(add_window, text="Submit", width=10, bd=5, font=("Arial", 12, "bold"), fg="White", bg="black", command=add_to_db)
-        submit.place(relx=0.4, y=350)
+            if not date.isdigit():
+                messagebox.showerror("Invalid date", "Year must be numbers only.")
+            
+            if (messagebox.askyesno("Confirm Criminal Record Add", "Do you wish to add the criminal record?")):
+                dbs.add_record(criminal_id, crime_id, location, date, blobplaceholder)
+                messagebox.showinfo("Successful Criminal Record Addition!", "Criminal Record was successfully added!")
+            
+        submit = tk.Button(frame, text="Submit", width=10, bd=5, font=("Arial", 12, "bold"), fg="White", bg="black" ,command=add_to_db)
+        submit.grid(row=5, column=1, padx=2, pady=10)
 
 dbs.connectDb()
 
@@ -69,98 +77,120 @@ root = tk.Tk()
 root.title("Interface")
 root.geometry("800x400+600+50")
 
+def addCrimeRecord():
+        add_window = tk.Toplevel()
+        add_window.title("Add Record")
+        add_window.geometry("800x400+550+200")
+        add_window.destroy()
+        adCrime = tk.Toplevel()
+        adCrime.title("Add New Crime ")
+        adCrime.geometry("800x800+550+100")
+        
+        title_label = tk.Label(adCrime, text="ADD NEW CRIME RECORD", font=("Arial", 20, "bold"))
+        title_label.place(relx=0.30, y=20)
+        
+        frame = tk.Frame(adCrime)
+        frame.place(relx=0.23, y=100)
+        
+        tk.Label(frame, text="Crime Name: ", font=("Arial", 14, "bold")).grid(row=1, column=0, padx=5, pady=20, sticky="e")
+        crime_name_entry = tk.Entry(frame, width=30, bd=3)
+        crime_name_entry.grid(row=1, column=1, padx=2, pady=10)
 
+        tk.Label(frame, text="Crime Sentence: ", font=("Arial", 14, "bold")).grid(row=2, column=0, padx=5, pady=20, sticky="e")
+        crime_sentence_entry = tk.Entry(frame, width=30, bd=3)
+        crime_sentence_entry.grid(row=2, column=1, padx=2, pady=10)
+
+        
+
+        def confirmAdd():
+            crime_name = crime_name_entry.get().strip()
+            crime_sentence = crime_sentence_entry.get().strip()
+            if not crime_name or not crime_sentence:
+                messagebox.showerror("Error", "Crime Name and Crime Sentence cannot be empty!")
+                return
+        
+            if  messagebox.askyesno("Confirm Adding Crime", f"Are you sure you want to add this crime?\nCrime Name: {crime_name}\n Sentence: {crime_sentence}"):
+                    dbs.add_crimes(crime_name, crime_sentence)
+                    messagebox.showinfo("Success", "New crime record added successfully!")
+                    adCrime.destroy()
+                
+        tk.Button(frame, text="Submit", width=15, bd=5, font=("Arial", 12, "bold"), fg="White", bg="black", command=confirmAdd).grid(row=4, column=1, padx=0, pady=20)
+        
 
 
 def editRecord():
-    edit_window = tk.Toplevel()
-    edit_window.title("Edit Record")
-    edit_window.geometry("900x900+450+40")
-    edit_title = tk.Label(edit_window, text="EDIT RECORD", font=("Arial", 20, "bold"))
-    edit_title.place(relx=0.35, y=20)
-    
-    frame = tk.Frame(edit_window)
-    frame.place(relx=0.20, y=80)
-    tk.Label(frame, text="Enter Criminal ID to edit: ", font=("Arial", 14, "bold")).grid(row=1, column=0, padx=10, pady=10, sticky="w")
-    criminalId = tk.Entry(frame, width=30, bd=3)
-    criminalId.grid(row=1, column=1, padx=220, pady=10)
+    if dbs.checkEmpty("records"):
+        messagebox.showerror("Empty records", "There are no records yet.")
 
-    def checkId():
-        crimId = criminalId.get().strip()
-        if not crimId.isdigit():    #checker if valid input(numbers only)
-
-            messagebox.showerror("Wrong input!", "Enter a number only.")
-            edit_window.focus_force() #napupunta sa likod ng main menu yung editwindow pag wala to
+    else: 
+        edit_window = tk.Toplevel()
+        edit_window.title("Edit Record")
+        edit_window.geometry("900x900+450+40")
+        edit_title = tk.Label(edit_window, text="EDIT RECORD", font=("Arial", 20, "bold"))
+        edit_title.place(relx=0.35, y=20)
         
-        else:
-            crimId = int(crimId)
-    
-            if not dbs.checkExist(crimId): #checks if inputted ID is on the database
-                messagebox.showerror("Criminal not found", "Criminal does not exist.")
+        frame = tk.Frame(edit_window)
+        frame.place(relx=0.20, y=80)
+        tk.Label(frame, text="Enter Criminal ID to edit: ", font=("Arial", 14, "bold")).grid(row=1, column=0, padx=10, pady=10, sticky="w")
+        criminalId = tk.Entry(frame, width=30, bd=3)
+        criminalId.grid(row=1, column=1, padx=220, pady=10)
+
+        def checkId():
+            crimId = criminalId.get().strip()
+            if not crimId.isdigit():    #checker if valid input(numbers only)
+
+                messagebox.showerror("Wrong input!", "Enter a number only.")
                 edit_window.focus_force() #napupunta sa likod ng main menu yung editwindow pag wala to
-            else:
-                criminalId.config(state="readonly")
-                submit.destroy()
-                crimData = dbs.getCriminalData(crimId)
-                 #Name update
-        
-
-                tk.Label(frame, text=f"Name: ", font=("Arial", 14, "bold")).grid(row=3, column=0, padx=10, pady=10, sticky="w")
-                criminalName = tk.Entry(frame, width=30, bd=3)
-                criminalName.grid(row=3, column=1, padx=220, pady=10)
-                criminalName.insert(0, crimData["name"])
-
-                    #Crime
-                crime_var = tk.StringVar()
-                crime_var.set(crimData["crime"])
-                tk.Label(frame, text="Crime: ", font=("Arial", 14, "bold")).grid(row=5, column=0, padx=10, pady=10, sticky="w")
-                crime_options = dbs.getCrimeList()
-                crime_dropdown = tk.OptionMenu(frame, crime_var, *crime_options)
-                crime_dropdown.grid(row=5, column=1, padx=220, pady=10)
-                crime_dropdown.config(width=24, bd=3)
             
+            else:
+                crimId = int(crimId)
+                if not dbs.checkExist(crimId): #checks if inputted ID is on the database
+                    messagebox.showerror("Criminal not found", "Criminal does not exist.")
+                    edit_window.focus_force() #napupunta sa likod ng main menu yung editwindow pag wala to
+                else:
+                    criminalId.config(state="readonly")
+                    submit.destroy()
+                    crimData = dbs.getCriminalData(crimId)
+            
+                #Location
+                    tk.Label(frame, text="Location: ", font=("Arial", 14, "bold")).grid(row=2, column=0, padx=10, pady=10, sticky="w")
+                    criminalLocation = tk.Entry(frame, width=30, bd=3)
+                    criminalLocation.grid(row=2, column=1, padx=2, pady=10)
+                    criminalLocation.insert(0, crimData["location"])
 
-              #Location
-                tk.Label(frame, text="Location: ", font=("Arial", 14, "bold")).grid(row=7, column=0, padx=10, pady=10, sticky="w")
-                criminalLocation = tk.Entry(frame, width=30, bd=3)
-                criminalLocation.grid(row=7, column=1, padx=2, pady=10)
-                criminalLocation.insert(0, crimData["location"])
 
+                    #Date
+                    tk.Label(frame, text="Year of Arrest: ", font=("Arial", 14, "bold")).grid(row=3, column=0, padx=10, pady=10, sticky="w")
+                    recordDate = tk.Entry(frame, width=30, bd=3)
+                    recordDate.grid(row=3,column=1, padx =2, pady=10)
+                    recordDate.insert(0, crimData["date"])
 
-                #Date
-                tk.Label(frame, text="Year of Arrest: ", font=("Arial", 14, "bold")).grid(row=9, column=0, padx=10, pady=10, sticky="w")
-                recordDate = tk.Entry(frame, width=30, bd=3)
-                recordDate.grid(row=9,column=1, padx =2, pady=10)
-                recordDate.insert(0, crimData["date"])
-
-             
-                def submitEdit():
-                    crimName = criminalName.get().strip()
-                    crimDesc = crime_var.get().strip()
-                    crimeLoc = criminalLocation.get().strip()
-                    crimeDate = recordDate.get().strip()
-                    if (messagebox.askokcancel("Confirm update", "Are you sure you want to update the record?")): 
-                        
-                        dbs.editRecord(crimId, crimName, crimDesc, crimeLoc, crimeDate)
-                        messagebox.showinfo("Update Success!", "Successfully updated the record!")
-                    else:
-                        edit_window.focus_force()
                 
-                def cancelEdit():
-                    if (messagebox.askyesno("Cancel Edit", "Are you sure you want to cancel editing? Unsaved changes will be lost.")):
-                        edit_window.destroy()
+                    def submitEdit():
+                        crimeLoc = criminalLocation.get().strip()
+                        crimeDate = recordDate.get().strip()
+                        if (messagebox.askokcancel("Confirm update", "Are you sure you want to update the record?")): 
+                            
+                            dbs.editRecord(crimId, crimeLoc, crimeDate)
+                            messagebox.showinfo("Update Success!", "Successfully updated the record!")
+                        else:
+                            edit_window.focus_force()
                     
+                    def cancelEdit():
+                        if (messagebox.askyesno("Cancel Edit", "Are you sure you want to cancel editing? Unsaved changes will be lost.")):
+                            edit_window.destroy()
+                        
 
 
-                update = tk.Button(frame, text="Update Record", font=("Arial", 10, "bold"), command=submitEdit)
-                cancel = tk.Button(frame, text="Cancel Update", font=("Arial", 10, "bold"), command=cancelEdit)
-                update.grid(row=10, column=1, padx=10,columnspan=2, pady=10, sticky="nw")
-                cancel.grid(row=11, column=1, padx=10, columnspan=2, pady=10, sticky="nw")
+                    update = tk.Button(frame, text="Update Record", font=("Arial", 10, "bold"), command=submitEdit)
+                    cancel = tk.Button(frame, text="Cancel Update", font=("Arial", 10, "bold"), command=cancelEdit)
+                    update.grid(row=4, column=1, padx=10,columnspan=2, pady=10, sticky="nw")
+                    cancel.grid(row=5, column=1, padx=10, columnspan=2, pady=10, sticky="nw")
 
 
-    submit = tk.Button(edit_window, text="Submit", width=15, bd=3, font=("Arial", 12), command=checkId)
-    submit.place(relx = 0.35, y=200)
-
+        submit = tk.Button(edit_window, text="Submit", width=15, bd=3, font=("Arial", 12), command=checkId)
+        submit.place(relx = 0.35, y=200)
+#end od editrecord
 
 
 def editCrime():
@@ -211,6 +241,8 @@ def addCriminal():
 
 label = tk.Label(root, text="CRIME RECORD MANAGEMENT", font=("Arial", 20, "bold"))
 label.pack(padx=10, pady=20)
+addCrime = tk.Button(root, text="Add Crime", width=30, bd=5, font=("Arial", 12, "bold"), fg="White", bg="black", comman=addCrimeRecord)
+addCrime.pack(padx=20, pady=20)
 addcrim = tk.Button(root, text="Add Criminal", width=30, bd=5, font=("Arial", 12, "bold"), fg="White", bg="black", command=addCriminal)
 addcrim.pack(padx=20, pady=20)
 add = tk.Button(root, text="Add Record", width=30, bd=5, font=("Arial", 12, "bold"), fg="White", bg="black", command=addRecord)
