f8eb51d564ac4b947f62ee893f973b2159ede732
Carl Mariño <97602174+carlmarinyo@users.noreply.github.com>
2025-03-27 16:27:58 +0800

    Update CriminalManage.py
    updated na yung add criminal(kasama na yung mugshot upload)

--- Python Final (magkakasama na)/CriminalManage.py
+++ Python Final (magkakasama na)/CriminalManage.py
@@ -3,6 +3,7 @@ import random
 import dbs
 from tkinter import messagebox
 import sqlite3
+from tkinter import filedialog
 
 class CriminalManage:
     def __init__(self, root):
@@ -22,10 +23,28 @@ class CriminalManage:
         criminalName = tk.Entry(frame, width=30, bd=3)
         criminalName.grid(row=1, column=1, padx=2, pady=10)
 
+        tk.Label(frame, text="Upload Mugshot: ", font=("Arial", 14, "bold")).grid(row=5, column=0, padx=5, pady=10, sticky="e")
+        
+        def upload_image():
+            global image_path
+            file_path = filedialog.askopenfilename(filetypes=[("Image Files", "*.png;*.jpg;*.jpeg")])
+            if file_path:
+                image_path = file_path
+                print(f"Selected Image: {file_path}")
+                
+                
+        uploadbtn = tk.Button(frame, text="Browse files", width=26, command=upload_image)
+        uploadbtn.grid(row=5, column=1, padx=5, pady=10)
+
         def submitEdit():
+            image_blob = None
+            if image_path:
+                with open(image_path, "rb") as file:
+                    image_blob = file.read()
+
             name = criminalName.get().strip()
             if messagebox.askyesno("Confirm Addition of Criminal", f"Do you wish to add {name} in the criminal list?"):
-                dbs.addCriminal(name)
+                dbs.addCriminal(name, image_blob)
                 messagebox.showinfo("Criminal Addition Success", f"{name} was added to the criminal list.")
                 criminal_window.destroy()
             
